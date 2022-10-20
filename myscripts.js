4b8c8bf91f8171a941b1dcdb2bae5b6243f4c5bb
Sam Tyson <tysonsam@msu.edu>
2022-10-20 10:46:41 -0400

    added fuctions

--- myscripts.js
+++ myscripts.js
@@ -4,12 +4,19 @@
 const options = ["rock", "paper", "scissors"];
 var getComputerChoice = options[Math.floor(Math.random()*options.length)];
 const computerSelection = getComputerChoice;
-const playerSelection = 'scissors';
+const playerSelection = "paper";
 
 //If statement for rock>scissors, paper>rock and scissors>paper
 
 function playRound(playerSelection, computerSelection) {
-    
+    if ((playerSelection == "scissors" && computerSelection == "rock") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "rock" && computerSelection == "paper")) {
+        console.log('You lose');
+    } else if ((playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "rock" && computerSelection == "scissors")) {
+        console.log('You win');
+    } else 
+        console.log('Tie');
 }
 
 console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
+console.log(playerSelection);
